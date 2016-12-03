374598afb45c3e487dc78e4d609ec77464f9e4d4
hsmtn <er.husam@gmail.com>
2016-12-03 17:02:35 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hsmtn/soru2/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.hsmtn.soru2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(getApplicationContext(),"Merhaba cevap 2",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b=(Button) findViewById(R.id.button);
+        b.setOnClickListener(this);
+
+    }
+}
